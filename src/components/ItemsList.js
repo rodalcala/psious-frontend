8682ed975b88b81acc4714146072ea1e50a56b6e
Rodrigo Alcala <alcala@myego2go.de>
2019-09-21 20:05:45 +0200

    feat: add logic to display completed tasks

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,6 +1,6 @@
 /* Component handling items logic. Will pass props to the items themselves. */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import Item from './Item'
@@ -10,18 +10,32 @@ import actions from '../redux/actions'
 import './ItemsList.css';
 
 const ItemsList = (props) => {
-  const renderItems = () => props.items.map(item => !item.completed &&
+  const [showCompleted, setShowCompleted] = useState(false);
+
+  const renderItems = () => props.items.map(item => (item.completed === showCompleted) &&
     <Item
       key={item._id}
       item={ item }
       completeItem={ props.completeItem }
       deleteItem={ props.deleteItem }
     />
-  )
+  );
+
+  const renderEmpty = () => <p className="ItemsList-noitems">You haven't submitted any task yet!</p>
+
+  const renderFooter = () =>
+    <div className="ItemsList-togleItemsContainer" >
+      <p className="ItemsList-toggleItems" onClick={() => setShowCompleted(!showCompleted)}>
+        { showCompleted ? 'Show pending tasks' : 'Show completed tasks'}
+      </p>
+    </div>
 
   return (
     <div className="ItemsList-container" >
-      { renderItems() }
+      <div className="ItemsList-itemsContainer" >
+        { props.items.length === 0 ? renderEmpty() : renderItems() }
+      </div>
+      { props.items.length === 0 ? null : renderFooter() }
     </div>
   );
 };
